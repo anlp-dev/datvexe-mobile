96810bafb9e811b67216ae1c11ac95987977600a
anlp-dev <anlp.sep19@gmail.com>
2025-03-05 13:59:04 +0700

    update code

--- src/api/ApiConfig.jsx
+++ src/api/ApiConfig.jsx
@@ -1,5 +1,5 @@
 const API_URL = "http://10.0.2.2:9999";
-const API_URL_IP = "https://5154-2405-4802-4b8-83a0-a038-e39f-e63c-4dbe.ngrok-free.app";
+const API_URL_IP = "http://54.206.126.69:9999";
 const API = "http://localhost:9999"
 const API_URL_DEPLOY = "https://datvexe-backend.onrender.com"
 
